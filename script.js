8cc0d41bff611abd1f28e1d8a87d8c15e8485486
Dhrumi Vyas <dhrumivyas20705@gmail.com>
2025-05-12 23:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,25 +13,25 @@ const testimonials = [
             text: "SoftSell made it incredibly easy to offload our unused licenses. We had money in our account within days—no hassle, no hidden fees.",
             name: "Jackson",
             role: "IT Director, Horizon Enterprises",
-            photo: "1.jpg"
+            photo: "img/1.jpg"
       },
       {
             text: "I wasn’t sure what to expect, but the process was smooth from start to finish. The support team was super responsive, too!",
             name: "Madison",
             role: "Small Business Owner",
-            photo: "2.jpg"
+            photo: "img/2.jpg"
       },
       {
             text: "As a growing company, every dollar counts. SoftSell helped us recover value from software we no longer needed—something we didn’t even know was possible.",
             name: "Ethan",
             role: "Operations Manager, NexaTech",
-            photo: "3.jpg"
+            photo: "img/3.jpg"
       },
       {
             text: "Highly recommended. Fast quotes, fair pricing, and zero pressure. I’ll definitely use SoftSell again and I'll recommend it to all",
             name: "Avery",
             role: "Systems Administrator",
-            photo: "4.jpeg"
+            photo: "img/4.jpeg"
       }
 ];
 
